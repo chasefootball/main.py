85fa1d4bc23857cde93506cb397def4e6d9b7a89
chasefootball <cdavis8@nic.edu>
2026-02-24 16:59:01 +0000

    this is the 3rd

--- HW.py
+++ HW.py
@@ -10,8 +10,17 @@ add = True
 # This is setting sub (subtract) = False
 sub = False
 
+
+chioce = input("would you like me to encrypt or decrypt for you? e/d ")
+if chioce == "e":
+    print("sure! I will encrypt for you!")
+if chioce == "d":
+    print("Oh boy you chose the hard way ")
+
 # This is asking the user for an input and giving plain_text a value
-plain_text = input("what would you like me to encrypt or decrypt for you? ")
+plain_text = input(f"What would you like me to {chioce} for you? ")
+
+shift_nums = 0
 
 # This is asking the user for an input and giving shift_key a value
 shift_key = int(input("What would you like to set your shift key to? "))
@@ -19,43 +28,53 @@ shift_key = int(input("What would you like to set your shift key to? "))
 # This is asking the user for an input and giving add_subtract a value
 add_subtract = input("would you like to add or subtract your shift key? add/sub ")
 
+if chioce == "e":
+    while True:
+        if add:
+            for letter in plain_text:
+                position = alphabet.find(letter)
+
+                if position == -1:
+                    cipher_text += letter
+
+                else:
+                    encrypt_position = position + shift_key
+                    if encrypt_position > len(alphabet)-1:
+                        encrypt_position %= len(alphabet)-1
+
+                    encrypt_position = position + shift_key
+
+                    encrypt_letter = alphabet[encrypt_position]
+
+                    cipher_text += encrypt_letter
+            print(f"Your cipher text is {cipher_text} ")
+            break
+        
+        if sub:
+            for letter in plain_text:
+                position = alphabet.find(letter)
+
+                if position == -1:
+                    cipher_text += letter
+
+                else:
+                    encrypt_position = position + shift_key
+                    if encrypt_position > len(alphabet)-1:
+                        encrypt_position -= len(alphabet)-1
+
+                    encrypt_position = position + shift_key
+
+                    encrypt_letter = alphabet[encrypt_position]
+
+                    cipher_text -= encrypt_letter
+
+        print(f"{plain_text} is decrypted to {cipher_text}")
+
 while True:
-    if add:
-        for letter in plain_text:
-            position = alphabet.find(letter)
+    if chioce == "d":
+        ask = input(f"{cipher_text}, is this readable? y/n ")
+        shift_nums += 1
 
-            if position == -1:
-                cipher_text += letter
-
-            else:
-                encrypt_position = position + shift_key
-                if encrypt_position > len(alphabet)-1:
-                    encrypt_position -= len(alphabet)-1
-
-                encrypt_position = position + shift_key
-
-                encrypt_letter = alphabet[encrypt_position]
-
-                cipher_text += encrypt_letter
-        print(f"Your cipher text is {cipher_text} ")
-        break
-    
-    if sub:
-        for letter in plain_text:
-            position = alphabet.find(letter)
-
-            if position == -1:
-                cipher_text += letter
-
-            else:
-                encrypt_position = position + shift_key
-                if encrypt_position > len(alphabet)-1:
-                    encrypt_position -= len(alphabet)-1
-
-                encrypt_position = position + shift_key
-
-                encrypt_letter = alphabet[encrypt_position]
-
-                cipher_text -= encrypt_letter
-
-    print(f"{plain_text} is decrypted to {cipher_text}")
+        if ask == "y":
+            print(f"we need the shift key {shift_nums} amount of times ")
+            break
